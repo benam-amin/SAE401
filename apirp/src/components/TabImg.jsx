0d028ef1c42edd0d975e748cdd70a9fb0a1399a7
benam-amin <benamaouche.amin@gmail.com>
2025-03-24 11:39:18 +0100

    modiciation nom des fichiers en jsx + supprimer imports react

---
+++ apirp/src/components/TabImg.jsx
@@ -0,0 +1,16 @@
+import './TabImg.css';
+
+const TabImg = ({ imageUrl, imageAlt, title, description }) => {
+    return (
+        <div className="tab-img-container">
+            <img src={imageUrl} alt={imageAlt} className="tab-img" />
+            
+            <div className="tab-content">
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default TabImg;
